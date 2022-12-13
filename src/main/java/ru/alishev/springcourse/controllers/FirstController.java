4e5108cb98d6c73803d20a9a9dab224498b83104
Kim1350 <rholmogorov12@gmail.com>
2022-12-13 11:05:52 +0900

    added less20

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +13,47 @@ import javax.servlet.http.HttpServletRequest;
 //@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
 //        String name = request.getParameter("name");
 //        String surname = request.getParameter("surname");
 
-        System.out.println("Hello, "+name + surname);
+//        System.out.println("Hello, "+name + surname);
 
-
+        model.addAttribute("message", "Hello: "+ name + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calc")
+    public String calc(@RequestParam("a") int a, @RequestParam("b") int b,
+                       @RequestParam("action") String action, Model model){
+
+        double result;
+
+        switch (action){
+            case  "*":
+                result = a*b;
+                break;
+            case "/":
+                result=a/(double)b;
+                break;
+            case "basd":
+                result= a+b;
+                break;
+            case "-":
+                result= a-b;
+                break;
+            default:
+                result=0;
+                break;
+        }
+
+        model.addAttribute("res", result);
+
+        return "first/calc";
     }
 }
